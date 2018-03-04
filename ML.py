e53871bf4b35830d49634b3f52b653b49f218e80
MuZhu <zhumu17@gmail.com>
2018-03-04 13:51:31 -0500

    updated

--- ML.py
+++ ML.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
