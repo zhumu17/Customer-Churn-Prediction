55a677c4c0bdda196746c1278bda451cea4f8a25
MuZhu <zhumu17@gmail.com>
2018-05-23 14:44:46 -0400

    updated

--- ML.py
+++ ML.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 # import seaborn as sns
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
 from sklearn.linear_model import LogisticRegression
